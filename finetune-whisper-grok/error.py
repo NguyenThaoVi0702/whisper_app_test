101eae40af0c19336be4ac34e1ffd465499c7c95
NguyenThaoVi0702 <105551975+NguyenThaoVi0702@users.noreply.github.com>
2025-10-09 11:31:08 +0700

    Update error.py

--- finetune-whisper-grok/error.py
+++ finetune-whisper-grok/error.py
@@ -1,61 +1,67 @@
-ai_dev@ppe-nvidia-k8s-worker01:/u01/user-data/vint1/finetune_whisper$ docker exec -it lora-finetuning-job bash
-ai_dev@a98c021bfbc1:/app$ ls
-Dockerfile         create_tokenizer.py  merge_lora.py         merged_model_dir  my-whisper-medium-lora  requirements.txt  run_workflow.sh  testfile       transcribe_faster_whisper.py
-convert_to_ct2.py  finetune_lora.py     merged_model_ct2_dir  model             remove_folder.bash      run_temp.sh       test_audio       transcribe.py
-ai_dev@a98c021bfbc1:/app$ python3 finetune_lora.py
+
+Original train size: 634158
+Filter:   0%|                                                                                                                                                             | 0/634158 [00:00<?, ? examples/s]
 Traceback (most recent call last):
-  File "/app/finetune_lora.py", line 64, in <module>
-    model = PeftModel.from_pretrained(model, ADAPTER_TO_CONTINUE_FROM, is_trainable=True)
-  File "/usr/local/lib/python3.10/dist-packages/peft/peft_model.py", line 525, in from_pretrained
-    model = cls(
-  File "/usr/local/lib/python3.10/dist-packages/peft/peft_model.py", line 132, in __init__
-    self.base_model = cls(model, {adapter_name: peft_config}, adapter_name)
-  File "/usr/local/lib/python3.10/dist-packages/peft/tuners/lora/model.py", line 142, in __init__
-    super().__init__(model, config, adapter_name, low_cpu_mem_usage=low_cpu_mem_usage)
-  File "/usr/local/lib/python3.10/dist-packages/peft/tuners/tuners_utils.py", line 180, in __init__
-    self.inject_adapter(self.model, adapter_name, low_cpu_mem_usage=low_cpu_mem_usage)
-  File "/usr/local/lib/python3.10/dist-packages/peft/tuners/tuners_utils.py", line 508, in inject_adapter
-    self._create_and_replace(peft_config, adapter_name, target, target_name, parent, current_key=key)
-  File "/usr/local/lib/python3.10/dist-packages/peft/tuners/lora/model.py", line 237, in _create_and_replace
-    new_module = self._create_new_module(lora_config, adapter_name, target, device_map=device_map, **kwargs)
-  File "/usr/local/lib/python3.10/dist-packages/peft/tuners/lora/model.py", line 318, in _create_new_module
-    from .bnb import dispatch_bnb_8bit
-  File "/usr/local/lib/python3.10/dist-packages/peft/tuners/lora/bnb.py", line 19, in <module>
-    import bitsandbytes as bnb
-  File "/usr/local/lib/python3.10/dist-packages/bitsandbytes/__init__.py", line 15, in <module>
-    from .nn import modules
-  File "/usr/local/lib/python3.10/dist-packages/bitsandbytes/nn/__init__.py", line 21, in <module>
-    from .triton_based_modules import (
-  File "/usr/local/lib/python3.10/dist-packages/bitsandbytes/nn/triton_based_modules.py", line 6, in <module>
-    from bitsandbytes.triton.dequantize_rowwise import dequantize_rowwise
-  File "/usr/local/lib/python3.10/dist-packages/bitsandbytes/triton/dequantize_rowwise.py", line 36, in <module>
-    def _dequantize_rowwise(
-  File "/usr/local/lib/python3.10/dist-packages/triton/runtime/autotuner.py", line 378, in decorator
-    return Autotuner(fn, fn.arg_names, configs, key, reset_to_zero, restore_value, pre_hook=pre_hook,
-  File "/usr/local/lib/python3.10/dist-packages/triton/runtime/autotuner.py", line 130, in __init__
-    self.do_bench = driver.active.get_benchmarker()
-  File "/usr/local/lib/python3.10/dist-packages/triton/runtime/driver.py", line 23, in __getattr__
-    self._initialize_obj()
-  File "/usr/local/lib/python3.10/dist-packages/triton/runtime/driver.py", line 20, in _initialize_obj
-    self._obj = self._init_fn()
-  File "/usr/local/lib/python3.10/dist-packages/triton/runtime/driver.py", line 9, in _create_driver
-    return actives[0]()
-  File "/usr/local/lib/python3.10/dist-packages/triton/backends/nvidia/driver.py", line 535, in __init__
-    self.utils = CudaUtils()  # TODO: make static
-  File "/usr/local/lib/python3.10/dist-packages/triton/backends/nvidia/driver.py", line 89, in __init__
-    mod = compile_module_from_src(Path(os.path.join(dirname, "driver.c")).read_text(), "cuda_utils")
-  File "/usr/local/lib/python3.10/dist-packages/triton/backends/nvidia/driver.py", line 58, in compile_module_from_src
-    cache = get_cache_manager(key)
-  File "/usr/local/lib/python3.10/dist-packages/triton/runtime/cache.py", line 277, in get_cache_manager
-    return __cache_cls(_base32(key))
-  File "/usr/local/lib/python3.10/dist-packages/triton/runtime/cache.py", line 69, in __init__
-    os.makedirs(self.cache_dir, exist_ok=True)
-  File "/usr/lib/python3.10/os.py", line 215, in makedirs
-    makedirs(head, exist_ok=exist_ok)
-  File "/usr/lib/python3.10/os.py", line 215, in makedirs
-    makedirs(head, exist_ok=exist_ok)
-  File "/usr/lib/python3.10/os.py", line 215, in makedirs
-    makedirs(head, exist_ok=exist_ok)
-  File "/usr/lib/python3.10/os.py", line 225, in makedirs
-    mkdir(name, mode)
-PermissionError: [Errno 13] Permission denied: '/home/ai_dev'
+  File "/app/finetune_lora.py", line 85, in <module>
+    train_dataset = train_dataset.filter(filter_data, num_proc=1)
+  File "/usr/local/lib/python3.10/dist-packages/datasets/arrow_dataset.py", line 557, in wrapper
+    out: Union["Dataset", "DatasetDict"] = func(self, *args, **kwargs)
+  File "/usr/local/lib/python3.10/dist-packages/datasets/fingerprint.py", line 442, in wrapper
+    out = func(dataset, *args, **kwargs)
+  File "/usr/local/lib/python3.10/dist-packages/datasets/arrow_dataset.py", line 3746, in filter
+    indices = self.map(
+  File "/usr/local/lib/python3.10/dist-packages/datasets/arrow_dataset.py", line 557, in wrapper
+    out: Union["Dataset", "DatasetDict"] = func(self, *args, **kwargs)
+  File "/usr/local/lib/python3.10/dist-packages/datasets/arrow_dataset.py", line 3079, in map
+    for rank, done, content in Dataset._map_single(**dataset_kwargs):
+  File "/usr/local/lib/python3.10/dist-packages/datasets/arrow_dataset.py", line 3525, in _map_single
+    for i, batch in iter_outputs(shard_iterable):
+  File "/usr/local/lib/python3.10/dist-packages/datasets/arrow_dataset.py", line 3475, in iter_outputs
+    yield i, apply_function(example, i, offset=offset)
+  File "/usr/local/lib/python3.10/dist-packages/datasets/arrow_dataset.py", line 3398, in apply_function
+    processed_inputs = function(*fn_args, *additional_args, **fn_kwargs)
+  File "/usr/local/lib/python3.10/dist-packages/datasets/arrow_dataset.py", line 6423, in get_indices_from_mask_function
+    num_examples = len(batch[next(iter(batch.keys()))])
+  File "/usr/local/lib/python3.10/dist-packages/datasets/formatting/formatting.py", line 280, in __getitem__
+    value = self.format(key)
+  File "/usr/local/lib/python3.10/dist-packages/datasets/formatting/formatting.py", line 380, in format
+    return self.formatter.format_column(self.pa_table.select([key]))
+  File "/usr/local/lib/python3.10/dist-packages/datasets/formatting/formatting.py", line 460, in format_column
+    column = self.python_features_decoder.decode_column(column, pa_table.column_names[0])
+  File "/usr/local/lib/python3.10/dist-packages/datasets/formatting/formatting.py", line 226, in decode_column
+    return self.features.decode_column(column, column_name) if self.features else column
+  File "/usr/local/lib/python3.10/dist-packages/datasets/features/features.py", line 2122, in decode_column
+    [decode_nested_example(self[column_name], value) if value is not None else None for value in column]
+  File "/usr/local/lib/python3.10/dist-packages/datasets/features/features.py", line 2122, in <listcomp>
+    [decode_nested_example(self[column_name], value) if value is not None else None for value in column]
+  File "/usr/local/lib/python3.10/dist-packages/datasets/features/features.py", line 1414, in decode_nested_example
+    return schema.decode_example(obj, token_per_repo_id=token_per_repo_id) if obj is not None else None
+  File "/usr/local/lib/python3.10/dist-packages/datasets/features/audio.py", line 188, in decode_example
+    array = librosa.to_mono(array)
+  File "/usr/local/lib/python3.10/dist-packages/lazy_loader/__init__.py", line 83, in __getattr__
+    attr = getattr(submod, name)
+  File "/usr/local/lib/python3.10/dist-packages/lazy_loader/__init__.py", line 82, in __getattr__
+    submod = importlib.import_module(submod_path)
+  File "/usr/lib/python3.10/importlib/__init__.py", line 126, in import_module
+    return _bootstrap._gcd_import(name[level:], package, level)
+  File "<frozen importlib._bootstrap>", line 1050, in _gcd_import
+  File "<frozen importlib._bootstrap>", line 1027, in _find_and_load
+  File "<frozen importlib._bootstrap>", line 1006, in _find_and_load_unlocked
+  File "<frozen importlib._bootstrap>", line 688, in _load_unlocked
+  File "<frozen importlib._bootstrap_external>", line 883, in exec_module
+  File "<frozen importlib._bootstrap>", line 241, in _call_with_frames_removed
+  File "/usr/local/lib/python3.10/dist-packages/librosa/core/audio.py", line 20, in <module>
+    from .convert import frames_to_samples, time_to_samples
+  File "/usr/local/lib/python3.10/dist-packages/librosa/core/convert.py", line 6, in <module>
+    from . import notation
+  File "/usr/local/lib/python3.10/dist-packages/librosa/core/notation.py", line 1015, in <module>
+    def __o_fold(d):
+  File "/usr/local/lib/python3.10/dist-packages/numba/core/decorators.py", line 225, in wrapper
+    disp.enable_caching()
+  File "/usr/local/lib/python3.10/dist-packages/numba/core/dispatcher.py", line 807, in enable_caching
+    self._cache = FunctionCache(self.py_func)
+  File "/usr/local/lib/python3.10/dist-packages/numba/core/caching.py", line 647, in __init__
+    self._impl = self._impl_class(py_func)
+  File "/usr/local/lib/python3.10/dist-packages/numba/core/caching.py", line 383, in __init__
+    raise RuntimeError("cannot cache function %r: no locator available "
+RuntimeError: cannot cache function '__o_fold': no locator available for file '/usr/local/lib/python3.10/dist-packages/librosa/core/notation.py'

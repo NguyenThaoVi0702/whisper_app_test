7ad9f3a95d923a52c20f3a41b81825e4b5cf1a9e
NguyenThaoVi0702 <105551975+NguyenThaoVi0702@users.noreply.github.com>
2025-10-10 14:47:46 +0700

    Update error.py

--- finetune-whisper-grok/error.py
+++ finetune-whisper-grok/error.py
@@ -1,13 +1,146 @@
-
-ai_dev@67fefbbf1024:/app$ python3 finetune_lora.py
-trainable params: 6,553,600 || all params: 815,431,680 || trainable%: 0.8037
-Resolving data files: 100%|███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 105/105 [00:00<00:00, 354305.65it/s]
-Loading dataset shards: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 91/91 [00:00<00:00, 1569.70it/s]
-Original train size: 634158
-Filtered train size: 633414
-Original val size: 7500
-Filtered val size: 7494
-/app/finetune_lora.py:188: FutureWarning: `tokenizer` is deprecated and will be removed in version 5.0.0 for `Seq2SeqTrainer.__init__`. Use `processing_class` instead.
-  trainer = Seq2SeqTrainer(
-Starting continued training with evaluation...
-  0%|                                    
+{
+  "best_global_step": null,
+  "best_metric": null,
+  "best_model_checkpoint": null,
+  "epoch": 0.040415266867059034,
+  "eval_steps": 200,
+  "global_step": 400,
+  "is_hyper_param_search": false,
+  "is_local_process_zero": true,
+  "is_world_process_zero": true,
+  "log_history": [
+    {
+      "epoch": 0.0025259541791911896,
+      "grad_norm": 1.0528565645217896,
+      "learning_rate": 4.800000000000001e-06,
+      "loss": 0.1677,
+      "step": 25
+    },
+    {
+      "epoch": 0.005051908358382379,
+      "grad_norm": 1.0061231851577759,
+      "learning_rate": 9.800000000000001e-06,
+      "loss": 0.1376,
+      "step": 50
+    },
+    {
+      "epoch": 0.0075778625375735684,
+      "grad_norm": 0.898628830909729,
+      "learning_rate": 9.466666666666667e-06,
+      "loss": 0.1068,
+      "step": 75
+    },
+    {
+      "epoch": 0.010103816716764758,
+      "grad_norm": 0.7446250915527344,
+      "learning_rate": 8.91111111111111e-06,
+      "loss": 0.0937,
+      "step": 100
+    },
+    {
+      "epoch": 0.012629770895955947,
+      "grad_norm": 0.672296941280365,
+      "learning_rate": 8.355555555555556e-06,
+      "loss": 0.0823,
+      "step": 125
+    },
+    {
+      "epoch": 0.015155725075147137,
+      "grad_norm": 0.7925171256065369,
+      "learning_rate": 7.800000000000002e-06,
+      "loss": 0.0869,
+      "step": 150
+    },
+    {
+      "epoch": 0.017681679254338327,
+      "grad_norm": 0.6122221350669861,
+      "learning_rate": 7.244444444444445e-06,
+      "loss": 0.0834,
+      "step": 175
+    },
+    {
+      "epoch": 0.020207633433529517,
+      "grad_norm": 0.6292484402656555,
+      "learning_rate": 6.688888888888889e-06,
+      "loss": 0.0724,
+      "step": 200
+    },
+    {
+      "epoch": 0.022733587612720704,
+      "grad_norm": 0.494079053401947,
+      "learning_rate": 6.133333333333334e-06,
+      "loss": 0.0708,
+      "step": 225
+    },
+    {
+      "epoch": 0.025259541791911894,
+      "grad_norm": 0.6743124723434448,
+      "learning_rate": 5.577777777777778e-06,
+      "loss": 0.0711,
+      "step": 250
+    },
+    {
+      "epoch": 0.027785495971103084,
+      "grad_norm": 0.5979343056678772,
+      "learning_rate": 5.022222222222223e-06,
+      "loss": 0.0703,
+      "step": 275
+    },
+    {
+      "epoch": 0.030311450150294274,
+      "grad_norm": 0.5862478613853455,
+      "learning_rate": 4.4666666666666665e-06,
+      "loss": 0.0712,
+      "step": 300
+    },
+    {
+      "epoch": 0.03283740432948546,
+      "grad_norm": 0.6269673705101013,
+      "learning_rate": 3.911111111111112e-06,
+      "loss": 0.0678,
+      "step": 325
+    },
+    {
+      "epoch": 0.035363358508676654,
+      "grad_norm": 0.4882247745990753,
+      "learning_rate": 3.3555555555555557e-06,
+      "loss": 0.0703,
+      "step": 350
+    },
+    {
+      "epoch": 0.03788931268786784,
+      "grad_norm": 0.6420568227767944,
+      "learning_rate": 2.8000000000000003e-06,
+      "loss": 0.07,
+      "step": 375
+    },
+    {
+      "epoch": 0.040415266867059034,
+      "grad_norm": 0.6627606153488159,
+      "learning_rate": 2.2444444444444445e-06,
+      "loss": 0.072,
+      "step": 400
+    }
+  ],
+  "logging_steps": 25,
+  "max_steps": 500,
+  "num_input_tokens_seen": 0,
+  "num_train_epochs": 1,
+  "save_steps": 200,
+  "stateful_callbacks": {
+    "TrainerControl": {
+      "args": {
+        "should_epoch_stop": false,
+        "should_evaluate": false,
+        "should_log": false,
+        "should_save": true,
+        "should_training_stop": false
+      },
+      "attributes": {}
+    }
+  },
+  "total_flos": 4.4033296564224e+19,
+  "train_batch_size": 16,
+  "trial_name": null,
+  "trial_params": null
+}

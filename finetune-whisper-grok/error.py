3bff75ae243fd2875ee91517a9d8d22838cf6dc0
NguyenThaoVi0702 <105551975+NguyenThaoVi0702@users.noreply.github.com>
2025-10-13 14:57:01 +0700

    Update error.py

--- finetune-whisper-grok/error.py
+++ finetune-whisper-grok/error.py
@@ -1,18 +1,12 @@
-
 ai_dev@2158b1d839aa:/app$ python3 finetune_lora.py
 trainable params: 6,553,600 || all params: 815,431,680 || trainable%: 0.8037
-Resolving data files: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████| 105/105 [00:00<00:00, 204078.74it/s]
-Loading dataset shards: 100%|███████████████████████████████████████████████████████████████████████████████████████████████████████| 91/91 [00:00<00:00, 1607.56it/s]
+Resolving data files: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████| 105/105 [00:00<00:00, 142019.32it/s]
+Loading dataset shards: 100%|███████████████████████████████████████████████████████████████████████████████████████████████████████| 91/91 [00:00<00:00, 1681.34it/s]
 Original train size: 634158
 Filtered train size: 633414
 Original val size: 7500
 Filtered val size: 7494
 Traceback (most recent call last):
-  File "/app/finetune_lora.py", line 159, in <module>
+  File "/app/finetune_lora.py", line 157, in <module>
     training_args = Seq2SeqTrainingArguments(
-  File "<string>", line 137, in __init__
-  File "/usr/local/lib/python3.10/dist-packages/transformers/training_args.py", line 1648, in __post_init__
-    raise ValueError(
-ValueError: --load_best_model_at_end requires the save and eval strategy to match, but found
-- Evaluation strategy: no
-- Save strategy: steps
+TypeError: Seq2SeqTrainingArguments.__init__() got an unexpected keyword argument 'evaluation_strategy'

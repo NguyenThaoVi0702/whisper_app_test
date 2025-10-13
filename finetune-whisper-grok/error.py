4341522a42f478c94f53277b4abbc930ebaafeed
NguyenThaoVi0702 <105551975+NguyenThaoVi0702@users.noreply.github.com>
2025-10-13 15:31:10 +0700

    Update error.py

--- finetune-whisper-grok/error.py
+++ finetune-whisper-grok/error.py
@@ -1,4 +1,3 @@
-
 ai_dev@2158b1d839aa:/app$ python3 finetune_lora.py
 trainable params: 6,553,600 || all params: 815,431,680 || trainable%: 0.8037
 Resolving data files: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████| 105/105 [00:00<00:00, 317521.21it/s]
@@ -14,3 +13,4 @@ Starting continued training with evaluation...
   warnings.warn(f"MatMul8bitLt: inputs will be cast from {A.dtype} to float16 during quantization")
 /usr/local/lib/python3.10/dist-packages/bitsandbytes/autograd/_functions.py:315: UserWarning: MatMul8bitLt: inputs will be cast from torch.bfloat16 to float16 during quantization
   warnings.warn(f"MatMul8bitLt: inputs will be cast from {A.dtype} to float16 during quantization")
+{'loss': 0.1688, 'grad_norm': 1.0315258502960205, 'learning_rate': 4.800000000000001e-06, 'epoch': 0.0}

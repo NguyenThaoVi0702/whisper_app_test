42598e02642ed52eb5ee0fd3288268e9319db3a7
NguyenThaoVi0702 <105551975+NguyenThaoVi0702@users.noreply.github.com>
2025-10-13 10:33:14 +0700

    Update error.py

--- finetune-whisper-grok/error.py
+++ finetune-whisper-grok/error.py
@@ -1,2 +1,2 @@
-RUN pip config set global.index-url https://dso-nexus.vietinbank.vn/repository/itd_pypi/simple
-RUN pip config set global.trusted-host dso-nexus.vietinbank.vn
+RUN pip config set global.index-url https://dso-nexus.*****.vn/repository/itd_pypi/simple
+RUN pip config set global.trusted-host dso-nexus.*****.vn
